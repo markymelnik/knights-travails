bca046053eb85314da0eda07f9ae97f883538a19
markymelnik <markemelnik@gmail.com>
2023-02-28 11:57:12 -0500

    Updated gameboard creation and added Node class

--- knightMoves.js
+++ knightMoves.js
@@ -1,11 +1,21 @@
 // Knights Travails
 
-const gameboard = new Map();
-
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    gameboard.set(`${i},${j}`, []);
+class Node {
+  constructor([x, y], moves, route) {
+    this.location = [x, y];
+    this.moves = moves;
+    this.route = route;
   }
 }
+const createGameboard = () => {
+  const gameboard = [];
+  for (let i = 0; i < 8; i++) {
+    gameboard[i] = [];
+    for (let j = 0; j < 8; j++) {
+      gameboard[i][j] = false;
+    }
+  }
+  return gameboard;
+}
 
-console.log(gameboard);
+console.log(createGameboard());
