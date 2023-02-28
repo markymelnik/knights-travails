1f61a412a7c11675ae05a318bd9c2d706cf3e4fe
markymelnik <markemelnik@gmail.com>
2023-02-28 17:30:10 -0500

    Added knightMoves function that returns the shortest path and displayKnightPath function that displays the path taken

--- knightMoves.js
+++ knightMoves.js
@@ -1,14 +1,14 @@
 // Knights Travails
 
 class Node {
-  constructor([x, y], moves, route) {
+  constructor([x, y], moves, path) {
     this.tile = [x, y];
     this.moves = moves;
-    this.route = route;
+    this.path = path;
   }
 }
 
-const createGameboard = () => {
+const newGameBoard = () => {
   const gameboard = [];
   for (let i = 0; i < 8; i++) {
     gameboard[i] = [];
@@ -30,7 +30,8 @@ const tileIsInBounds = (tile) => {
 const tileIsEmpty = (board, tile) => {
   const x = tile[0];
   const y = tile[1];
-  return !board[x][y];
+  if (!board[x][y]) return board;
+  
 }
 
 const validMoves = (board, tile) => {
@@ -42,7 +43,7 @@ const validMoves = (board, tile) => {
     [tile[0] - 1, tile[1] - 2],
     [tile[0] - 2, tile[1] - 1],
     [tile[0] - 2, tile[1] + 1],
-    [tile[0] - 1, tile[1] + 2]
+    [tile[0] - 1, tile[1] + 2],
   ];
   let availableMoves = moves.filter((move) => {
     if (tileIsInBounds(tile) && tileIsInBounds(move) && tileIsEmpty(board, move)) {
@@ -52,7 +53,42 @@ const validMoves = (board, tile) => {
   return availableMoves;
 }
 
-let newBoard = createGameboard();
-console.log(newBoard);
-console.log(validMoves(newBoard, [1,2]));
-console.log(validMoves(newBoard, [0,8]));
+const displayKnightPath = (path) => {
+  let result = `You made it in ${path.moves} moves! Here's your path:`;
+  console.log(result);
+  path.path.forEach((tile) => {
+    console.log(tile);
+  })
+}
+
+const knightMoves = (startTile, endTile) => {
+  let currentBoard = newGameBoard();
+  let moveQueue = [new Node([startTile[0], startTile[1]], 0, [[startTile[0], startTile[1]]])];
+  while (moveQueue.length > 0) {
+    let currentMove = moveQueue.shift();
+    if (
+      currentMove.tile[0] === endTile[0] && 
+      currentMove.tile[1] === endTile[1]
+    ) {
+      return displayKnightPath(currentMove);
+    } else {
+      let availableTiles = validMoves(currentBoard, currentMove.tile);
+      availableTiles.forEach((tile) => {
+        let nextTile = new Node(
+          [tile[0], tile[1]], 
+          currentMove.moves + 1, 
+          currentMove.path.concat([tile])
+        );
+        moveQueue.push(nextTile);
+      })
+    }
+  }
+}
+
+knightMoves([3,3],[4,3]);
+
+// You made it in 3 moves! Here's your path:
+// > [3,3]
+// > [4,5]
+// > [6,4]
+// > [4,3]
